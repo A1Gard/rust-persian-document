57d396eda65f82da1c504fbe09e7f5f572fc2092
A1Gard <a1gard@4xmen.ir>
2025-12-09 15:55:21 +0330

    fix: pagination scroll next to top

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Happy developing ✨')
+
+document.addEventListener('click', function(e) {
+    const item = e.target.closest('.pagination-item');
+    if (!item) return;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
